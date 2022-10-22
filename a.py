3ff47f4dd056bee0c3af19b55c786c3949e0cd0b
AKWA-GAIUS <115952527+AKWA-GAIUS@users.noreply.github.com>
2022-10-22 17:54:08 +0300

    Update a.py

--- a.py
+++ a.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import base64  #to open .gif files in streamlit app
 
 st.title("Akwa Visualization App For Daywise Dataset")
 st.header("Covid Visualization")
@@ -22,7 +21,7 @@ def get_value(val,my_dict):
 
 
 st.image("covidd.jpg")
-data=pd.read_csv("C:/Users/USER/OneDrive/Desktop/covid/day_wise.csv")
+data=pd.read_csv("day_wise.csv")
 numeric_columns = list(data.select_dtypes(['float', 'int']).columns)
 non_numeric_columns = list(data.select_dtypes(['object']).columns)
 non_numeric_columns.append(None)
